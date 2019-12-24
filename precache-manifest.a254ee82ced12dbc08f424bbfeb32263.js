d659d76f80c4f20dc127d2b7babfa5c8b6bb7c33
chizoba <chizyberto@gmail.com>
2019-12-24 16:45:51 +0100

    Updates

---
+++ precache-manifest.a254ee82ced12dbc08f424bbfeb32263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfe1d342f2f790a654088cd72667f49",
+    "url": "/monster-catalog/index.html"
+  },
+  {
+    "revision": "9e6357e63048ae673e4b",
+    "url": "/monster-catalog/static/css/main.fee64140.chunk.css"
+  },
+  {
+    "revision": "d392583b04fbbf82cf29",
+    "url": "/monster-catalog/static/js/2.5623ebb2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-catalog/static/js/2.5623ebb2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e6357e63048ae673e4b",
+    "url": "/monster-catalog/static/js/main.c2157b7f.chunk.js"
+  },
+  {
+    "revision": "63e2c2dc48149271b4a0",
+    "url": "/monster-catalog/static/js/runtime-main.380ae495.js"
+  }
+]);
